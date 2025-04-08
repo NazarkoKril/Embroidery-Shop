801eafe1871c5992ebc9cc1cf1336789f6905230
Valleriiia <168529644+Valleriiia@users.noreply.github.com>
2025-04-09 01:59:02 +0300

    home page pc

--- assets/js/script.js
+++ assets/js/script.js
@@ -19,3 +19,15 @@ document.addEventListener('DOMContentLoaded', () => {
         }
     
     });
+
+    document.addEventListener("DOMContentLoaded", function () {
+        new Swiper("#swiper", {
+            slidesPerView: "auto", 
+            spaceBetween: 10, 
+            loop: true,
+            navigation: {
+                nextEl: ".swiper-button-next",
+                prevEl: ".swiper-button-prev",
+            },
+        });
+    });
